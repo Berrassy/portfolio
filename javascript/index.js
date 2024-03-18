97916edaeb754650a170059c70699b18eeecef93
youssef berrassy <youssefberrassy26@gmail.com>
2024-03-18 10:37:32 +0000

    first one

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+function sendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_quxtupt";
+  const templateID = "template_d2hzgmb";
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((err) => console.log(err));
+}
